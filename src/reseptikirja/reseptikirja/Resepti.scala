cd2a32bd02256024109d017bb545682dfce59da6
Rasmus <rasmus.sipari@aalto.fi>
2015-05-06 13:10:11 +0300

    Käyty läpi, palautuskunnossa.

--- src/reseptikirja/reseptikirja/Resepti.scala
+++ src/reseptikirja/reseptikirja/Resepti.scala
@@ -4,7 +4,6 @@ package reseptikirja/**
  * @author Rasmus
  */
 class Resepti(val nimi: String, val aineet:Vector[(String,Double,String)], val ohje: String) {
-  
   override def toString = {
     var tuloste = nimi + "\n\nAineet:\n"
     for (x <- aineet) {
