294e8e5106101ded2bc140a4235318cccca47910
Rasmus <rasmus.sipari@aalto.fi>
2015-05-06 13:09:41 +0300

    Käyty läpi. Palautuskunnossa

--- src/reseptikirja/reseptikirja/Aine.scala
+++ src/reseptikirja/reseptikirja/Aine.scala
@@ -5,7 +5,7 @@ package reseptikirja
  * @author Rasmus
  */
 
-// Tiheys on g/litra
+// 
 class Aine(val nimi: String,  var maara: Double, val yksikko: String, val tiheys: Int, val allergeeni: String) {
   override def toString = {
     nimi+", " +maara+", "+ yksikko+ ", " + tiheys +", " +allergeeni
